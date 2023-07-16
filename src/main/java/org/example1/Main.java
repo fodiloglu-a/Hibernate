02ac466b63f8d700f5a94115ad845798f858ec51
fodiloglu-a <mutalip.fodiloglu@gmail.com>
2023-07-16 20:47:41 +0300

    Session and Session Factory ant firs DB create

--- src/main/java/org/example1/Main.java
+++ src/main/java/org/example1/Main.java
@@ -1,9 +1,35 @@
 package org.example1;
 
+import org.example1.entity.UserModel;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        UserModel userModel=new UserModel();
+        userModel.setUserName("Test User");
+
+        SessionFactory sessionFactory=new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(UserModel.class)
+                .buildSessionFactory();
+
+         Session session=sessionFactory.openSession();
+        try {
+
+            Transaction transaction= session.getTransaction();
+            transaction.begin();
+            session.merge(userModel);
+            transaction.commit();
+
+        }catch (Exception e){
+            System.out.println("Confugration Exception"+e);
+        }finally {
+            session.close();
+        }
+
 
     }
 }
