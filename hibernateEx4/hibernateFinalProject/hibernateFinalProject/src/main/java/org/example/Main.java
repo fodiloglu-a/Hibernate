efa0ed9128441fbf56daeda1a8e76c37c058614d
fodiloglu-a <mutalip.fodiloglu@gmail.com>
2023-07-21 16:18:41 +0300

    Example4 final

---
+++ hibernateEx4/hibernateFinalProject/hibernateFinalProject/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.example.hibernate.HibernateUtil;
+import org.hibernate.Session;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ hibernateEx4/hibernateFinalProject/hibernateFinalProject/src/main/java/org/example/hibernate/HibernateUtil.java
@@ -0,0 +1,23 @@
+package org.example.hibernate;
+
+
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    public static final SessionFactory sessionFactory=buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            SessionFactory sessionFactory=new Configuration()
+                    .configure("hibernate.cfg.xml")
+
+                    .buildSessionFactory();
+            return sessionFactory;
+
+        }catch (Exception e){
+            throw new RuntimeException();
+        }
+    }
+}
